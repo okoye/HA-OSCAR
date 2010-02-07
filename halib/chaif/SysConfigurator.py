95dfe544043ead4fba46650f8723288c4013578b
okoye <okoye9@hotmail.com>
2010-02-06 18:18:45 -0600

    fixed bugs in system configurator

--- halib/chaif/SysConfigurator.py
+++ halib/chaif/SysConfigurator.py
@@ -57,8 +57,8 @@ class SysConfigurator:
                             #TODO: Ensure only alphanumeric characters
          sec_config['HOSTNAME'] = temp
       database_driver = ddriver.DbDriver()
-      test = database_driver.select_db("Primary_Configuration")['NIC_INFO']
-      print test
+      sec_config['NIC_INFO'] = database_driver.select_db("Primary_Configuration").pop()["NIC_INFO"]
+      sec_config = 
       sec_config['IP_ADDR'] = raw_input("enter a valid ip addr for secondary server: ")
       return sec_config
     

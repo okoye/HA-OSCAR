6b1eca52ab4c82be43bf518807b3dc2b05445dc0
Chuka Okoye <okoye9@hotmail.com>
2010-03-01 19:26:09 -0600

    completely absorbed systemimager dir

--- binaries/ha_systemimager/packages-install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-aptitude install libappconfig-perl libxml-simple-perl mkisofs mtools atftpd atftp pxe grub || exit 0;
-
-ln -s /usr/bin/atftp /usr/bin/tftp
-dpkg -i systemconfigurator_*.deb
-dpkg -i systemimager-common_*.deb systemimager-boot-i386-standard_*.deb systemimager-server_*.deb
-dpkg -i   systemimager-initrd-template-i386* systemimager-client_*
-
-

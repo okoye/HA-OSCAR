c75159de878989e3976721cff103012a4d9c9367
root <root@test-1.(none)>
2010-04-26 15:32:35 -0400

    add some more dependencies + remove bittorrent and flamethrower

--- binaries/download-packages.sh
+++ binaries/download-packages.sh
@@ -6,4 +6,4 @@
 ./install -v --download-only --tag stable --directory . systemconfigurator \
 systemimager-client systemimager-common \
 systemimager-boot-i386-standard systemimager-initrd-template-i386 \
-systemimager-server systemimager-server-bittorrent systemimager-server-flamethrowerd
+systemimager-server #systemimager-server-bittorrent systemimager-server-flamethrowerd
--- binaries/packages-install.sh
+++ binaries/packages-install.sh
@@ -2,7 +2,7 @@
 # TO BE REMOVED WHEN THE PACKAGING IS DONE &
 # THE PACKAGE SERVER IS UP 
 # 
-aptitude install libappconfig-perl libxml-simple-perl mkisofs mtools atftpd atftp pxe grub || exit 0;
+aptitude install libappconfig-perl libxml-simple-perl mkisofs mtools atftpd atftp pxe grub binutils || exit 0;
 
 ln -s /usr/bin/atftp /usr/bin/tftp
 dpkg -i systemconfigurator_*.deb
